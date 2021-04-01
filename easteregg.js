4dcad499cd689c178faa053960c9f2a615887119
TheRookie14 <mayesjake6@gmail.com>
2021-04-01 12:55:35 +0100

    Update easteregg.js

--- easteregg.js
+++ easteregg.js
@@ -12,7 +12,21 @@ var keyHandler = function (event) {
 
 	if (pattern.length === current) {
 		current = 0;
-		window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+		rr = document.createElement("iframe");
+		rr.frameBorder = "0";
+		rr.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+		rr.width = "100%";
+		rr.height="100%";
+		rr.style.zIndex = "-2";
+		rr.style.position = "fixed";
+		rr.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1";
+		document.getElementsByTagName("body")[0].appendChild(rr);
+		var x = document.getElementById("videoBG");
+		x.style.animationFillMode = "forwards";
+		x.animate([{opacity:1},{opacity:0}],2500);
+		setTimeout(function(){ x.parentNode.removeChild(x); }, 2500);
+
+
 	}
 
 };
